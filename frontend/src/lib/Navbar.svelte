c4dde9c064b521c3d8fafe30dbde30914550441c
lumirth <65358837+lumirth@users.noreply.github.com>
2023-04-09 12:58:35 -0500

    refactor UI into svelte components

---
+++ frontend/src/lib/Navbar.svelte
@@ -0,0 +1,48 @@
+<script>
+    /* Add any necessary JavaScript */
+  </script>
+  
+  <style>
+    nav {
+      display: flex;
+      justify-content: center;
+    }
+  
+    nav ul {
+      list-style: none;
+      display: flex;
+      align-items: center;
+    }
+  
+    nav li {
+      margin: 0 10px;
+    }
+  
+    nav button {
+      padding: 8px 16px;
+      background-color: #4CAF50;
+      color: white;
+      border: none;
+      border-radius: 4px;
+    }
+  
+    nav a {
+      color: #333;
+      text-decoration: none;
+      padding: 8px 16px;
+    }
+  
+    nav a:hover {
+      background-color: #ddd;
+    }
+  </style>
+  
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><button>Test</button></li>
+    </ul>
+  </nav>
+  
