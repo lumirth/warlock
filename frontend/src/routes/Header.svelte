c908d2d94e9c5729ba6d5d3e89812c9e1cbc5f32
lumirth <65358837+lumirth@users.noreply.github.com>
2023-04-23 17:31:46 -0500

    add course warlock logo

--- frontend/src/routes/Header.svelte
+++ frontend/src/routes/Header.svelte
@@ -1,14 +1,24 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
+	import warlock from '$lib/images/warlock.svg';
 </script>
 
 <header class="mb-10 md:mb-20 lg:mb-30 xl:mb-40 ">
 	<nav class="h-16 border-b border-neutral bg-base-200 w-auto justify-between">
     <div class="navbar w-auto justify-between">
       <div class="flex space-x-0">
-        <a href="/" class="btn btn-ghost normal-case text-xl">COURSE WARLOCK</a>
+        <a href="/" class="flex items-center">
+          <span class="btn btn-ghost normal-case text-xl">
+            <img src="{warlock}" alt="Warlock logo" class="h-9 w-9 mr-1 ml-[-9px]">
+            <!-- two rows -->
+            <!-- left-aligned -->
+            <!-- small gap -->
+            <div class="flex flex-col text-left gap-0 leading-5">
+              <span class="block">COURSE</span>
+              <span class="block">WARLOCK</span>
+            </div>
+          </span>
+        </a>
       </div>
       <div class="flex-none">
           <a class="btn font-normal text-lg border-none bg-transparent" href="/about">ABOUT</a>
@@ -17,4 +27,7 @@
 </header>
 
 <style>
+	img {
+		fill: currentColor;
+	}
 </style>
