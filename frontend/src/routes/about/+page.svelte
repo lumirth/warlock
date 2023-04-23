e3e3c842b4d59ec9f8e7ba028ac2f032d60d3755
lumirth <65358837+lumirth@users.noreply.github.com>
2023-04-23 03:44:10 -0500

    add sharp corners to about page

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -14,7 +14,7 @@
 		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. It was created by running
 	</p>
 
-	<pre>npm create svelte@latest</pre>
+	<pre class="!rounded-none">npm create svelte@latest</pre>
 
 
 </div>
