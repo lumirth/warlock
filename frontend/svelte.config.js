90d83cec0f9c08c7016ef89cec7b1fe10bc463ec
lumirth <65358837+lumirth@users.noreply.github.com>
2023-04-23 03:34:56 -0500

    add todos

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+// TODO: add static adapter
+// TODO: add favicons
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
