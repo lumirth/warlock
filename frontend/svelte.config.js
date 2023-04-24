41e8945f923e757c87373ada9054c72aeee93c72
lumirth <65358837+lumirth@users.noreply.github.com>
2023-04-24 01:28:54 -0500

    add static adapter

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 // TODO: add static adapter
 // TODO: add favicons
