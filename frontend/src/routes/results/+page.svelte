a2913f5f22a1cf1ca1807a5ee1a54318a63c18aa
lumirth <65358837+lumirth@users.noreply.github.com>
2023-04-23 19:33:52 -0500

    add results route to be modified later

---
+++ frontend/src/routes/results/+page.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+	<title>Results - Course Warlock</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="prose sm:prose-sm md:prose-md lg:prose-lg mx-auto">
+	<h1>ABOUT THIS APP</h1>
+
+	<p>
+		Course Warlock brings together RateMyProfessor ratings, historic GPA data, and the University of Illinois's courses API together to give one straightforward and comprehensive tool for understanding whether a course is worth taking. 
+	</p>
+
+	<p>
+		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. It was created by running:
+	</p>
+
+	<pre class="!rounded-none bg-base-200">npm create svelte@latest</pre>
+
+
+</div>
