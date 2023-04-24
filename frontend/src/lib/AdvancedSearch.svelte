dcf72502d9e46f16fbfe511da2842a6e8eef0007
lumirth <65358837+lumirth@users.noreply.github.com>
2023-04-24 00:36:30 -0500

    make advancedsearch use fields from API

--- frontend/src/lib/AdvancedSearch.svelte
+++ frontend/src/lib/AdvancedSearch.svelte
@@ -4,129 +4,57 @@
   import NumberInput from "./NumberInput.svelte";
   import Checkbox from "./Checkbox.svelte";
 
-  // Replace the following arrays with the appropriate data
-  const yearOptions = [
-    { value: "2023", text: "2023" },
-    { value: "2022", text: "2022" },
-    { value: "2021", text: "2021" },
-    { value: "2020", text: "2020" },
-    { value: "2019", text: "2019" },
-    { value: "2018", text: "2018" },
-    { value: "2017", text: "2017" },
-    { value: "2016", text: "2016" },
-    { value: "2015", text: "2015" },
-    { value: "2014", text: "2014" },
-    { value: "2013", text: "2013" },
-    { value: "2012", text: "2012" },
-    { value: "2011", text: "2011" },
-    { value: "2010", text: "2010" },
-    { value: "2009", text: "2009" },
-    { value: "2008", text: "2008" },
-    { value: "2007", text: "2007" },
-    { value: "2006", text: "2006" },
-    { value: "2005", text: "2005" },
-    { value: "2004", text: "2004" },
-  ];
+  import yearsJson from "../data/years.json";
+  import termsJson from "../data/terms.json";
+  import collegesJson from "../data/colleges.json";
+  import subjectsJson from "../data/subjects.json";
+  import genEdsJson from "../data/gen_eds.json";
+  import potsJson from "../data/pots.json";
 
-  const semesterOptions = [
-    { value: "spring", text: "Spring" },
-    { value: "fall", text: "Fall" },
-    { value: "summer", text: "Summer" },
-    { value: "winter", text: "Winter" },
-  ];
+  // Create arrays using imported JSON data
+  const yearOptions = Object.entries(yearsJson).map(([text, value]) => ({
+    text,
+    value: String(value),
+  }));
+  const semesterOptions = Object.entries(termsJson).map(([text, value]) => ({
+    text,
+    value: String(value),
+  }));
+  const collegeOptions = Object.entries(collegesJson).map(([text, value]) => ({
+    text,
+    value: String(value),
+  }));
+  const subjectOptions = Object.entries(subjectsJson).map(([text, value]) => ({
+    text,
+    value: String(value),
+  }));
+  const genedReqsOptions = Object.entries(genEdsJson).map(([text, value]) => ({
+    text,
+    value: String(value),
+  }));
+  const partOfTermOptions = Object.entries(potsJson).map(([text, value]) => ({
+    text,
+    value: String(value),
+  }));
 
+  // Define selected value variables for each dropdown
+  let selectedYear: string;
+  let selectedSemester: string;
+  let selectedCollege: string;
+  let selectedSubject: string;
+  let selectedGenedReqs1: string;
+  let selectedGenedReqs2: string;
+  let selectedGenedReqs3: string;
+  let selectedPartOfTerm: string;
+  let selectedKeywordType: string;
+  let selectedCourseLevel: string;
+
+  // Create additional options arrays (not using JSON files)
   const keywordTypeOptions = [
     { value: "default", text: "Default" },
     { value: "exact phrase", text: "Exact Phrase" },
     { value: "any of these words", text: "Any Words" },
     { value: "all of these words", text: "All Words" },
-  ];
-
-  const collegeOptions = [
-    {
-      value: "ACES",
-      text: "Agricultural, Consumer and Environmental Sciences",
-    },
-    { value: "AHS", text: "Applied Health Sciences" },
-    { value: "AFROTC", text: "Armed Forces" },
-    { value: "CARLE", text: "Carle Illinois College of Medicine" },
-    { value: "MEDIA", text: "College of Media" },
-    { value: "DGS", text: "Division of General Studies" },
-    { value: "EDUC", text: "Education" },
-    { value: "FAA", text: "Fine and Applied Arts" },
-    { value: "BUS", text: "Gies College of Business" },
-    { value: "GRAD", text: "Graduate College" },
-    { value: "ENG", text: "Grainger College of Engineering" },
-    { value: "LAW", text: "Law" },
-    { value: "LAS", text: "Liberal Arts and Sciences" },
-    { value: "iSchool", text: "School of Information Sciences" },
-    { value: "LER", text: "School of Labor and Employee Relations" },
-    { value: "SSW", text: "School of Social Work" },
-    { value: "VETMED", text: "Veterinary Medicine" },
-  ];
-
-  const subjectOptions = [
-    { value: "AAS", text: "Asian American Studies" },
-    { value: "ABE", text: "Agricultural and Biological Engineering" },
-    { value: "ACCY", text: "Accountancy" },
-    { value: "ACE", text: "Agricultural and Consumer Economics" },
-    { value: "ACES", text: "Agricultural, Consumer and Environmental Sciences" },
-    { value: "ADV", text: "Advertising" },
-    { value: "AE", text: "Aerospace Engineering" },
-    { value: "AFAS", text: "Air Force Aerospace Studies" },
-    { value: "AFRO", text: "African American Studies" },
-    { value: "AFST", text: "African Studies" },
-    { value: "AGCM", text: "Agricultural Communications" },
-    { value: "AGED", text: "Agricultural Education" },
-    { value: "AGLL", text: "Agricultural Leadership and Science Education" },
-    { value: "AGRO", text: "Agroecology and Sustainable Food Systems" },
-    { value: "AHS", text: "Applied Health Sciences" },
-    { value: "AIS", text: "American Indian Studies" },
-    { value: "ANSC", text: "Animal Sciences" },
-    { value: "ANTH", text: "Anthropology" },
-    { value: "ARAB", text: "Arabic" },
-    { value: "ARCH", text: "Architecture" },
-    { value: "ART", text: "Art" },
-    { value: "ARTD", text: "Art and Design" },
-    { value: "ARTF", text: "Art Foundations" },
-    { value: "ARTS", text: "Art and Art History" },
-    { value: "ASRM", text: "Actuarial Science and Risk Management" },
-    { value: "ASTR", text: "Astronomy" },
-    { value: "ATMS", text: "Atmospheric Sciences" },
-    { value: "BADM", text: "Business Administration" },
-    { value: "BASQ", text: "Basque" },
-    { value: "BCOG", text: "Brain and Cognitive Science" },
-    { value: "BCS", text: "Bosnian-Croatian-Serbian" },
-    { value: "BIOC", text: "Biochemistry" },
-    { value: "BIOE", text: "Bioengineering" },
-    { value: "BIOL", text: "Biology" },
-    { value: "BIOP", text: "Biophysics" },
-    { value: "BSE", text: "Biomedical Sciences and Engineering" },
-    { value: "BTW", text: "Business and Technical Writing" },
-    { value: "BULG", text: "Bulgarian" },
-    { value: "BUS", text: "Business" },
-    { value: "C&I", text: "Curriculum and Instruction" },
-    { value: "CEE", text: "Civil and Environmental Engineering" },
-    { value: "CFS", text: "Child and Family Studies" },
-    { value: "CHBE", text: "Chemical and Biomolecular Engineering" },
-    { value: "CHEM", text: "Chemistry" },
-    { value: "CHIN", text: "Chinese" },
-    { value: "CHLH", text: "Community Health" },
-    { value: "CHP", text: "Campus Honors Program" },
-    { value: "CI", text: "Curriculum and Instruction" },
-    { value: "CINE", text: "Cinema Studies" },
-    { value: "CITL", text: "Center for Innovation in Teaching and Learning" },
-    { value: "CLCV", text: "Classical Civilization" },
-    { value: "CMN", text: "Communication" },
-    { value: "CPSC", text: "Crop Sciences" },
-    { value: "CS", text: "Computer Science" },
-    { value: "CSE", text: "Computer Science and Engineering" },
-    { value: "CW", text: "Creative Writing" },
-    { value: "CWL", text: "Comparative and World Literature" },
-    { value: "CZCH", text: "Czech" },
-    { value: "DANC", text: "Dance" },
-    { value: "DTX", text: "Human-Centered Design and Design Thinking" },
-    { value: "EALC", text: "East Asian Languages and Cultures" },
   ];
 
   const sectionAttributesOptions = [
@@ -141,48 +69,6 @@
     { value: "level4", text: "400 Level" },
     { value: "level5", text: "500 Level" },
   ];
-
-  const genedReqsOptions = [
-    { value: "COMP1", text: "Composition I" },
-    { value: "1CLL", text: "Advanced Composition" },
-    { value: "HUM", text: "Humanities and the Arts" },
-    { value: "1LA", text: "Humanities and the Arts: Literature & the Arts" },
-    {
-      value: "1HP",
-      text: "Humanities and the Arts: Historical & Philosophical Perspectives",
-    },
-    { value: "1LS", text: "Natural Sciences and Technology: Life Science" },
-    { value: "1PS", text: "Natural Sciences and Technology: Physical Science" },
-    { value: "1QR1", text: "Quantitative Reasoning I" },
-    { value: "1QR2", text: "Quantitative Reasoning II" },
-    { value: "1SS", text: "Social Science" },
-    { value: "1BSC", text: "Behavioral Science" },
-    { value: "CS", text: "Cultural Studies" },
-    { value: "1WCC", text: "Western/Comparative Cultures" },
-    { value: "1NW", text: "Non-Western Cultures" },
-    { value: "1US", text: "US Minority Cultures" },
-  ];
-
-  const partOfTermOptions = [
-    { value: "fullterm", text: "Full Term" },
-    { value: "first8weeks", text: "First 8 Weeks" },
-    { value: "last8weeks", text: "Last 8 Weeks" },
-    { value: "part1", text: "Part 1" },
-    { value: "part2", text: "Part 2" },
-  ];
-
-  // Define selected value variables for each dropdown
-  let selectedYear: string;
-  let selectedSemester: string;
-  let selectedKeywordType: string;
-  let selectedCollege: string;
-  let selectedSubject: string;
-  let selectedSectionAttributes: string;
-  let selectedCourseLevel: string;
-  let selectedGenedReqs1: string;
-  let selectedGenedReqs2: string;
-  let selectedGenedReqs3: string;
-  let selectedPartOfTerm: string;
 
   let keywordValue: string;
   let instructorValue: string;
@@ -233,6 +119,9 @@
   <NumberInput id="courseId" label="Course ID" value={courseIdValue} />
   <NumberInput id="crn" label="CRN" value={crnValue} />
   <NumberInput id="creditHours" label="Credit Hours" value={creditHoursValue} />
+  <!-- TODO: add section attributes back. this would require adding maintenance
+  scripts to fetch attributes. the query parameter for it is not documented. it
+  is degreeAtt -->
   <!-- <Dropdown
     id="sectionAttributes"
     label="Section Attributes"
