58b85c2ef3bbcb84eb13ed9cbf31def2f0017cce
lumirth <65358837+lumirth@users.noreply.github.com>
2023-04-30 06:26:27 -0500

    Updates

--- _app/immutable/chunks/0-c82a260f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-3d24b334.js";export{t as component};
---
+++ _app/immutable/chunks/2-c6d7311d.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-da46b06b.js";import{default as t}from"../components/pages/_page.svelte-f911954a.js";export{t as component,r as universal};
