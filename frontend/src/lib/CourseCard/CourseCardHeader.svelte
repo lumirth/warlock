71ab47f76c8bbcb278fb84b14de31fa6e7e2511a
lumirth <65358837+lumirth@users.noreply.github.com>
2023-05-02 10:53:49 -0500

    ask windows to be nice to me

--- frontend/src/lib/CourseCard/CourseCardHeader.svelte
+++ frontend/src/lib/CourseCard/CourseCardHeader.svelte
@@ -31,7 +31,7 @@
   <div class="text-lg font-semibold whitespace-nowrap border-r border-neutral pr-3 pl-1 h-full flex flex-col justify-center decoration-1 underline underline-offset-2 transition-all hover:underline-offset-4 hover:text-primary">
     <a class="cursor-pointer" href={link}>{course_id}</a>
   </div>
-  <div class="relative overflow-x-scroll pl-1 pr-1 box-content">
+  <div class="relative overflow-x-auto pl-1 pr-1 box-content">
     <div class="absolute top-0 left-1 h-full w-4 bg-gradient-to-r from-base-200 to-transparent transition-opacity ease-in-out {sleft > 0 ? 'opacity-100' : 'opacity-0'}"></div>
     <div class="whitespace-nowrap overflow-x-scroll no-scrollbar scroll" bind:this={headerMiddleText} on:scroll={() => (sleft = headerMiddleText.scrollLeft)}>
       <div class="text-lg text-neutral font-light uppercase">{course_detail}&nbsp;&nbsp;&nbsp;</div>
