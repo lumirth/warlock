97529c048d0777bc848b07b07e09f486aab90602
lumirth <65358837+lumirth@users.noreply.github.com>
2023-05-02 06:39:19 -0500

    edit about page

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -1,16 +1,33 @@
 <svelte:head>
-	<title>About - Course Warlock</title>
-	<meta name="description" content="About this app" />
+  <title>About - Course Warlock</title>
+  <meta name="description" content="About this app" />
 </svelte:head>
 
 <div class="prose sm:prose-sm md:prose-md lg:prose-lg mx-auto">
-	<h1>ABOUT THIS APP</h1>
+  <h1>ABOUT THIS APP</h1>
 
-	<p>
-		Course Warlock brings together the University of Illinois' course catalog, historic GPA data, and RateMyProfessor ratings(coming soon) together to give the most straightforward and comprehensive tool for understanding whether a course is worth taking. 
-	</p>
+  <p>
+    Course Warlock brings together the <a
+      class="decoration-1 underline underline-offset-2 transition-all hover:underline-offset-4 hover:text-primary"
+      href="https://courses.illinois.edu/cisdocs/api"
+      >University of Illinois' course API</a
+    >,
+    <a
+      class="decoration-1 underline underline-offset-2 transition-all hover:underline-offset-4 hover:text-primary"
+      href="https://github.com/wadefagen/datasets/tree/master/gpa"
+      >historic GPA data</a
+    >, and <a
+	class="decoration-1 underline underline-offset-2 transition-all hover:underline-offset-4 hover:text-primary"
+	href="https://www.ratemyprofessors.com/"
+	>RateMyProfessors ratings</a
+  > together to give an efficient and straightforward tool for understanding whether a course is
+    worth taking.
+  </p>
 
-	<p>
-		A <a class="decoration-1 underline underline-offset-2 transition-all hover:underline-offset-4 hover:text-primary" href="https://mirth.cc">lumirth</a> tool. Made for students, by students.
-
+  <p>
+    A <a
+      class="decoration-1 underline underline-offset-2 transition-all hover:underline-offset-4 hover:text-primary"
+      href="https://mirth.cc">lumirth</a
+    > tool.
+  </p>
 </div>
