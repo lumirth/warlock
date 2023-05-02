d2e6b41c0805cb3fd2f99304092f710eefafaf6a
lumirth <65358837+lumirth@users.noreply.github.com>
2023-05-02 17:48:37 -0500

    add fading examples to search bar

--- frontend/src/lib/SearchForm.svelte
+++ frontend/src/lib/SearchForm.svelte
@@ -1,40 +1,46 @@
-<script lang="ts">
-  import { createEventDispatcher, onMount } from "svelte";
+<script lang='ts'>
+  import { createEventDispatcher, onMount } from 'svelte';
   import { dev } from '$app/environment';
+  import { fade } from 'svelte/transition';
 
-  export let query = "";
+  export let query = '';
   export let loading = false;
   export let returned_results: boolean = false;
   export let results: any = [];
 
   const examples = [
-    "comp sci",
-    "CS 222",
-    "math257",
-    "pysch 100",
-    "mus",
-    "MACS, western",
-    "is:online, hrs:2",
-    "gen:ADVCOMP, is:online",
-    "prof: fagen-ulmschneider",
-    "life sciences",
-    "gened: QR2, gened: NAT",
-    "PSYC, behavioral science",
-    "fall, 2004, phil",
-    "anthropology, 2019",
-    "2005, mathematics, hrs:2",
-    "hrs:3, phil, nonwestern",
-    "pot:b, us minority",
-    "bus 3, is:open",
-    "astronomy",
-    "prof: cole",
+    'Try typing a course title',
+    'Try typing a course ID',
+    'Try typing a CRN',
+    'Try typing the name of a GenEd',
+    'Try typing a keyword',
+    '"comp sci"',
+    '"CS 222"',
+    '"math257"',
+    '"pysch 100"',
+    '"mus"',
+    '"MACS, western"',
+    '"is:online, hrs:2"',
+    '"gen:ADVCOMP, is:online"',
+    '"prof: fagen-ulmschneider"',
+    '"life sciences"',
+    '"gen: QR2, gen: NAT"',
+    '"PSYC, behavioral science"',
+    '"fall, 2004, phil"',
+    '"anthropology, 2019"',
+    '"2005, mathematics, hrs:2"',
+    '"hrs:3, phil, nonwestern"',
+    '"pot:b, us minority"',
+    '"bus 3, is:open"',
+    '"astronomy"',
+    '"prof: cole"',
   ];
 
-  let placeholder_example = "";
+  let placeholder_example = '';
 
   const randomExample = () => {
     placeholder_example =
-      '"' + examples[Math.floor(Math.random() * examples.length)] + '"';
+      examples[Math.floor(Math.random() * examples.length)];
   };
 
   const dispatch = createEventDispatcher();
@@ -45,14 +51,14 @@
     loading = true;
     queryBackend();
     event.preventDefault();
-    dispatch("submit", query);
+    dispatch('submit', query);
   };
 
   const queryBackend = async () => {
     try {
-      let url: string = "";
-      if (dev) url = "http://localhost:8000/search/simple?query=";
-      else url = "https://warlock-backend.fly.dev/search/simple?query=";
+      let url: string = '';
+      if (dev) url = 'http://localhost:8000/search/simple?query=';
+      else url = 'https://warlock-backend.fly.dev/search/simple?query=';
       const response = await fetch(
         url + encodeURIComponent(query)
       );
@@ -73,20 +79,40 @@
   onMount(() => {
     randomExample();
     // code to execute when the component is mounted
+    setInterval(() => {
+      randomExample();
+    }, 2000);
   });
 </script>
 
-<form on:submit={handleSubmit} class="flex gap-4 mx-auto sm:mt-0">
-  <input
-    type="text"
-    placeholder={placeholder_example}
-    class="flex-grow input hover:border-primary input-bordered w-2/3 md:w-4/5 lg:w-4/5 xl:w-5/6 bg-base-200 text-lg font-normal placeholder-neutral focus:outline-none focus:border-primary focus:placeholder-transparent"
-    bind:value={query}
-  />
+<form on:submit={handleSubmit} class='flex gap-4 mx-auto sm:mt-0'>
+  <div class='relative flex-grow'>
+    <input
+      type='text'
+      class='flex-grow input hover:border-primary input-bordered w-full bg-base-200 text-lg font-normal placeholder-neutral focus:outline-none focus:border-primary focus:placeholder-transparent peer'
+      bind:value={query}
+    />
+    <!-- hide when peer is not empty-->
+    <div class='absolute top-0 left-0 w-full peer-focus:opacity-0 pointer-events-none {query === '' ? 'opacity-100' : 'opacity-0'}'>
+      <!-- use fade-->
+      {#each [placeholder_example] as example (example)}
+        <span
+          in:fade={{ duration: 250, delay: 250 }}
+          out:fade={{ duration: 250 }}
+          class='text-lg font-normal text-neutral absolute w-11/12 top-1.5 left-1.5 px-3 py-1 select-none overflow-x-hidden whitespace-nowrap'
+        >
+          {example}
+        </span>
+      {/each}
+    </div>
+    <!-- well-placed placeholder div -->
+  </div>
+
+  
   {#if loading}
-    <button class="btn bg-base-200 font-normal text-lg loading">LOADING</button>
+    <button class='btn bg-base-200 font-normal text-lg loading'>LOADING</button>
   {:else}
-    <button type="submit" class="btn bg-base-200 font-normal text-lg"
+    <button type='submit' class='btn bg-base-200 font-normal text-lg'
       >SEARCH</button
     >
   {/if}
